e1d1144e59be9ad6c5841a93c8e0aa8f228e67b0
Ferooo00oo <107183703+Ferooo00oo@users.noreply.github.com>
2024-12-14 19:30:05 +0200

    Update TheFeedback.java

--- Mohamed's/TheFeedback.java
+++ Mohamed's/TheFeedback.java
@@ -2,12 +2,13 @@ package com.mycompany.project;
 
 import java.util.List;
 import java.util.ArrayList;
+
 public class TheFeedback {
-    protected static int []Feedback (String SC , String trys){
+    protected static int[] Feedback(String SC , String trys){
         int blackPegs = 0;
         int whitePegs = 0;
-        List<Character> sList = new ArrayList<>();
-        List<Character> tList = new ArrayList<>();
+        List<Character> secretList = new ArrayList<>();
+        List<Character> trysList = new ArrayList<>();
         
         //Black pegs
         for (int i = 0; i < att.getCODE_LENGTH(); i++) {
@@ -15,16 +16,16 @@ public class TheFeedback {
                 blackPegs++;
             }
             else {
-                sList.add(SC.charAt(i));
-                tList.add(trys.charAt(i));
+                secretList.add(SC.charAt(i));
+                trysList.add(trys.charAt(i));
             }
         }
         
         //White pegs
-        for (int i = 0; i < tList.size(); i++) {
-            if (sList.contains(tList.get(i))) {
+        for (int i = 0; i < trysList.size(); i++) {
+            if (secretList.contains(trysList.get(i))) {
                 whitePegs++;
-                sList.remove(tList.get(i));
+                secretList.remove(trysList.get(i));
             }
         }
         
