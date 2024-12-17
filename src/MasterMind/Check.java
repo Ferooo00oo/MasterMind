32151fb5c58a7bee87b197d23b6e76e2bc3c303f
Ferooo00oo <107183703+Ferooo00oo@users.noreply.github.com>
2024-12-18 01:55:05 +0200

    Update Check.java

--- src/MasterMind/Check.java
+++ src/MasterMind/Check.java
@@ -4,7 +4,7 @@ public class Check {
     
         protected static boolean IsValidCode(String code){
             String[] codeArray = code.split(" ");
-            if (codeArray.length == Att.getCODE_LENGTH()) {
+            if (codeArray.length == Att.getCodeLength()) {
                 for (String c : codeArray) {
                     if (Att.getColors()[0].equalsIgnoreCase(c) || Att.getColors()[1].equalsIgnoreCase(c) || Att.getColors()[2].equalsIgnoreCase(c) || Att.getColors()[3].equalsIgnoreCase(c) || Att.getColors()[4].equalsIgnoreCase(c) || Att.getColors()[5].equalsIgnoreCase(c)) {}
                     else return false ;
